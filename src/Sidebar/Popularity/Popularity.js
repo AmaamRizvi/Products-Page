855fe17162baaae592f56d52af88f4506b0aee68
Amaam_Rizvi <amaamrizvi6557@gmail.com>
2024-09-07 15:28:52 +0530

    new updates

---
+++ src/Sidebar/Popularity/Popularity.js
@@ -0,0 +1,68 @@
+// import "./Popularity.css";
+// import Input from "../../components/Input";
+
+// const Popularity = ({ handleChange }) => {
+//   return (
+//     <div>
+//       <h2 className="sidebar-title popularity-title">Popularity</h2>
+//       <label className="sidebar-label-container">
+//         <input
+//           onChange={handleChange}
+//           type="radio"
+//           value=""
+//           name="popularity"
+//         />
+//         <span className="checkmark all"></span>
+//         All
+//       </label>
+
+//       <Input
+//         handleChange={handleChange}
+//         value="0-10000"
+//         title="0-10,000"
+//         name="popularity"
+//         color="gray"
+//       />
+
+//       <Input
+//         handleChange={handleChange}
+//         value="10000-30000"
+//         title="10,000-30,000"
+//         name="popularity"
+//         color="gray"
+//       />
+
+//       <Input
+//         handleChange={handleChange}
+//         value="30000-50000"
+//         title="30,000-50,000"
+//         name="popularity"
+//         color="gray"
+//       />
+
+//       <Input
+//         handleChange={handleChange}
+//         value="50000+"
+//         title="50,000+"
+//         name="popularity"
+//         color="gray"
+//       />
+
+//       <label className="sidebar-label-container">
+//         <input
+//           onChange={handleChange}
+//           type="radio"
+//           value="100000+"
+//           name="popularity"
+//         />
+//         <span
+//           className="checkmark"
+//           style={{ background: "gray", border: "2px solid black" }}
+//         ></span>
+//         100,000+
+//       </label>
+//     </div>
+//   );
+// };
+
+// export default Popularity;
